58b304e115a0933114a92f0b1824bd692f6a35bb
vherever <vherever@gmail.com>
2017-01-28 22:40:13 +0200

    AboutHigherOrderFunctions, AboutObjects

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(2016);
-    expect(megalomaniac.calculateAge()).toBe(46);
+    expect(currentYear).toBe(2017);
+    expect(megalomaniac.calculateAge()).toBe(47);
   });
 
   describe("'in' keyword", function () {
